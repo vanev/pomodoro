e78897875391450976690de6959d2cb594437ee5
Evan Siegel <siegel.evan@gmail.com>
2017-12-02 23:49:22 -0500

    Elm and Webpack Stuff

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+const port  = process.env.PORT || 8080
+
+module.exports = {
+  entry: {
+    app: [
+      './src/index.js',
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?name=[name].[ext]',
+      },
+      {
+        test:    /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader:  'elm-webpack-loader?verbose=true&warn=true',
+      },
+    ],
+
+    noParse: /\.elm$/,
+  },
+
+  devServer: {
+    inline: true,
+    stats: { colors: true },
+    allowedHosts: [
+      'pomodoro.dev',
+      'pomodoro.test',
+    ],
+    port: port,
+  },
+}
